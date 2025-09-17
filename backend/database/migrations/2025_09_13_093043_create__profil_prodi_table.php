f242de19f2c76179965f90b002d0ad6c11159132
chxxlk <chrislekpey06@gmail.com>
2025-09-18 03:47:30 +0700

    fix: migration dan seeder ke db, jangan lupa untuk membuat table chat_history

--- backend/database/migrations/2025_09_13_093043_create__profil_prodi_table.php
+++ backend/database/migrations/2025_09_13_093043_create__profil_prodi_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('_profil_prodi', function (Blueprint $table) {
+        Schema::create('profil_prodi', function (Blueprint $table) {
             $table->id('id_prodi');
             $table->text('visi');
             $table->text('misi');
